0cd1424062945803a55b9455751245be8fb98624
Anjana Paul <anjanapaul0614@gmail.com>
2024-04-20 15:35:26 +0530

    ViewTextbook Component

--- etomep2/src/components/god/viewtextbook/ViewTextbook.jsx
+++ etomep2/src/components/god/viewtextbook/ViewTextbook.jsx
@@ -3,13 +3,15 @@ import { Modal, Button, Container, Row, Col } from "react-bootstrap";
 import { Link } from "react-router-dom";
 import chemistryImage from "../../../assets/chemistry.png";
 import { AiOutlineClose } from "react-icons/ai";
+import "../viewtextbook/viewtextbook.css";
+ 
 
 function ViewTextbook({ show, handleClose }) {
   return (
     <div style={{}}>
       <Container>
-        <Modal show={show} onHide={handleClose} centered>
-          <div style={{ width: "100%", height: "100%",  }}>
+        <Modal show={show} onHide={handleClose} centered  size="lg" >
+          <div style={{}}>
             <div
               style={{
                 display: "flex",
@@ -26,9 +28,9 @@ function ViewTextbook({ show, handleClose }) {
             {/* <Modal.Header closeButton >
             <Modal.Title>Chemistry Textbook for Class XII</Modal.Title>
             </Modal.Header> */}
-            <div style={{ display: "flex", justifyContent: "center" }}>
+            <div style={{ display: "flex",  }}>
                 <Row md={12}>
-                    <Col md={6} style={{border:'1px solid #526D82', width:'200px', height:'240px', display:'flex',  justifyContent:'center'}}>
+                    <Col md={6} style={{border:'0.2px solid #526D82', width:'200px', height:'240px', display:'flex',  justifyContent:'center', marginLeft:'40px'}}>
                     <div style={{ }}>
                         {/* <div> */}
                         <img
@@ -43,36 +45,36 @@ function ViewTextbook({ show, handleClose }) {
                         {/* </div> */}
                     </div>
                     </Col>
-                    <Col md={6}>
-                            
-                        <div style={{ }}>
+                    <Col md={6}>   
+                        <div >
                             <Modal.Body style={{ }}>
-                            <h5> TextBook Title </h5>
+                            <h5 style={{display:'flex', alignContent:'center', justifyContent:'center', marginBottom:'20px'}}> TextBook Title </h5>
+                            <div style={{ }}>
                             <Row>
                             <Col md={6}>
-                            <div style={{ }}>
+                            <div style={{color:'#727272', fontSize:'17px' }}>
                                 <p> Class </p>
-                                <p> Publication Name </p>
+                                <p style={{width:''}}> Publication </p>
                                 <p> Volume</p>
-                                <p> Total No. of Chapters</p>
+                                <p> Total</p>
                             </div>
                             </Col>
                             <Col md={6}>
-                            <div style={{ }}>
+                            <div style={{ color:'#000000', fontSize:'17px' }}>
                                 <p> Class </p>
-                                <p> Publication Name </p>
+                                <p> Publication </p>
                                 <p> Volume II </p>
-                                <p> Total No. of Chapters</p>
+                                <p> Total </p>
                             </div>
                             </Col>
                             </Row>
+                            </div>
                             </Modal.Body>
                         </div>
-                    </Col>
-                    
+                    </Col>  
               </Row>
             </div>
-            <div style={{marginLeft:'20px', marginRight:"20px", marginBottom:'20px', }}>
+            <div style={{marginLeft:'20px', marginRight:"20px", marginBottom:'20px',marginTop:'20px' }}>
             {/* <Modal.Footer> */}
                 <Row>
                     <Col style={{marginLeft:'40px',}}>
