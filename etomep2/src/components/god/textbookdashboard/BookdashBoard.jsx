1eab39e3be1dbb9f91355ffe132730c0fc79a1e5
Afif <mk.afifmuhammad@gmail.com>
2024-05-14 11:32:45 +0530

    added classs name dynamically in textbookdashboard

--- etomep2/src/components/god/textbookdashboard/BookdashBoard.jsx
+++ etomep2/src/components/god/textbookdashboard/BookdashBoard.jsx
@@ -1,13 +1,22 @@
-import React, { useState, useEffect } from 'react';
-import { BsFilterRight, BsSearch } from 'react-icons/bs';
-import { useSelector } from 'react-redux';
+import React, { useState, useEffect } from "react";
+import { BsFilterRight, BsSearch } from "react-icons/bs";
+import { useSelector } from "react-redux";
 import {
-  Container, Row, Nav, Navbar, NavDropdown, Form, Card, Col, Pagination
-} from 'react-bootstrap';
-import axios from 'axios';
-import { BiBookAdd } from 'react-icons/bi';
-import { Link } from 'react-router-dom';
-import ViewTextbook from '../viewtextbook/ViewTextbook';
+  Container,
+  Row,
+  Nav,
+  Navbar,
+  NavDropdown,
+  Form,
+  Card,
+  Col,
+  Pagination,
+} from "react-bootstrap";
+
+import axios from "axios";
+import { BiBookAdd } from "react-icons/bi";
+import { Link } from "react-router-dom";
+import ViewTextbook from "../viewtextbook/ViewTextbook";
 
 function BookdashBoard() {
   const [books, setBooks] = useState([]);
@@ -16,24 +25,37 @@ function BookdashBoard() {
   const [currentPage, setCurrentPage] = useState(1);
   const [booksPerPage] = useState(8);
   const [searchTerm, setSearchTerm] = useState("");
-  const APIURL = useSelector(state => state.APIURL.url);
+  const APIURL = useSelector((state) => state.APIURL.url);
+
+  console.log(books, "booksssssssss");
+
+  const getClassNames = () => {
+    const classNames = new Set();
+    books.forEach((book) => {
+      if (book.class_name) {
+        classNames.add(book.class_name);
+      }
+    });
+    return Array.from(classNames);
+  };
 
   useEffect(() => {
-    axios.get(`${APIURL}/api/create-textbook/`)
-      .then(response => {
+    axios
+      .get(`${APIURL}/api/create-textbook/`)
+      .then((response) => {
         setBooks(response.data);
       })
-      .catch(error => {
+      .catch((error) => {
         console.error("Error fetching books:", error);
       });
   }, []);
 
-  const handleCardClick = book => {
+  const handleCardClick = (book) => {
     setSelectedBook(book);
     setShowModal(true);
   };
 
-  const filteredBooks = books.filter(book =>
+  const filteredBooks = books.filter((book) =>
     book.text_name.toLowerCase().includes(searchTerm.toLowerCase())
   );
 
@@ -41,18 +63,41 @@ function BookdashBoard() {
   const indexOfFirstBook = indexOfLastBook - booksPerPage;
   const currentBook = filteredBooks.slice(indexOfFirstBook, indexOfLastBook);
 
-  const paginate = pageNumber => setCurrentPage(pageNumber);
+  const paginate = (pageNumber) => setCurrentPage(pageNumber);
 
   return (
-    <div style={{ backgroundColor: "#DDE6ED", minHeight: "100vh", overflowY: "auto" }}>
-      <Navbar expand="lg" className="bg-body-tertiary book" style={{ marginBottom: "20px", marginTop: '10px', borderRadius: "17px" }}>
+    <div
+      style={{
+        backgroundColor: "#DDE6ED",
+        minHeight: "100vh",
+        overflowY: "auto",
+      }}
+    >
+      <Navbar
+        expand="lg"
+        className="bg-body-tertiary book"
+        style={{
+          marginBottom: "20px",
+          marginTop: "10px",
+          borderRadius: "17px",
+        }}
+      >
         <Container>
-          <Navbar.Brand href="#home" style={{ color: '#526D82', fontSize: '23px' }}>Library</Navbar.Brand>
+          <Navbar.Brand
+            href="#home"
+            style={{ color: "#526D82", fontSize: "23px" }}
+          >
+            Library
+          </Navbar.Brand>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
           <Navbar.Collapse id="basic-navbar-nav">
             <Nav className="me-auto">
               <NavDropdown title="Class" id="basic-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">XII</NavDropdown.Item>
+                {getClassNames().map((className, index) => (
+                  <NavDropdown.Item key={index} href="#action/3.1">
+                    {className}
+                  </NavDropdown.Item>
+                ))}
               </NavDropdown>
               <NavDropdown title="Subject" id="basic-nav-dropdown">
                 <NavDropdown.Item href="#action/3.1">Science</NavDropdown.Item>
@@ -63,15 +108,22 @@ function BookdashBoard() {
             </Nav>
             <Form className="d-flex">
               <div className="position-relative">
-                <BsSearch className="position-absolute top-50 translate-middle-y ms-3" style={{ height: "20px", width: "20px", color: "#D8D4D4" }} />
+                <BsSearch
+                  className="position-absolute top-50 translate-middle-y ms-3"
+                  style={{ height: "20px", width: "20px", color: "#D8D4D4" }}
+                />
                 <Form.Control
                   type="search"
                   placeholder="Search by name"
                   className="me-5 ps-5"
                   aria-label="Search"
-                  style={{ width: "500px", borderRadius: "12px", fontWeight: "400px" }}
+                  style={{
+                    width: "500px",
+                    borderRadius: "12px",
+                    fontWeight: "400px",
+                  }}
                   value={searchTerm}
-                  onChange={e => setSearchTerm(e.target.value)}
+                  onChange={(e) => setSearchTerm(e.target.value)}
                 />
               </div>
             </Form>
@@ -79,11 +131,24 @@ function BookdashBoard() {
         </Container>
       </Navbar>
       <Container>
-        <Container style={{ backgroundColor: "#fff", borderRadius: "17px", marginBottom: "70px" }}>
+        <Container
+          style={{
+            backgroundColor: "#fff",
+            borderRadius: "17px",
+            marginBottom: "70px",
+          }}
+        >
           {currentBook.length === 0 ? (
             <Row className="justify-content-center">
               <Col>
-                <Card className="text-center p-4" style={{ backgroundColor: 'transparent', border: 'none', color: '#526D82' }}>
+                <Card
+                  className="text-center p-4"
+                  style={{
+                    backgroundColor: "transparent",
+                    border: "none",
+                    color: "#526D82",
+                  }}
+                >
                   <h5>No data to be shown</h5>
                 </Card>
               </Col>
@@ -103,7 +168,12 @@ function BookdashBoard() {
                     }}
                     onClick={() => handleCardClick(book)}
                   >
-                    <Card.Img variant="top" src={book.textbook_image} style={{ width: "150px", margin: "10px" }} class="image-container" />
+                    <Card.Img
+                      variant="top"
+                      src={book.textbook_image}
+                      style={{ width: "150px", margin: "10px" }}
+                      class="image-container"
+                    />
                     <Card.Body>
                       <Card.Title>{book.text_name}</Card.Title>
                       <Card.Text>{book.publisher_name}</Card.Text>
@@ -114,24 +184,59 @@ function BookdashBoard() {
             </Row>
           )}
           {currentBook.length > 0 && (
-            <Pagination className="book_pagination_custom" style={{ justifyContent: 'center' }}>
-              <Pagination.Prev onClick={() => paginate(currentPage - 1)} disabled={currentPage === 1} />
-              {Array.from({ length: Math.ceil(filteredBooks.length / booksPerPage) }, (_, i) => (
-                <Pagination.Item key={i} active={i + 1 === currentPage} onClick={() => paginate(i + 1)}>
-                  {i + 1}
-                </Pagination.Item>
-              ))}
-              <Pagination.Next onClick={() => paginate(currentPage + 1)} disabled={currentPage === Math.ceil(filteredBooks.length / booksPerPage)} />
+            <Pagination
+              className="book_pagination_custom"
+              style={{ justifyContent: "center" }}
+            >
+              <Pagination.Prev
+                onClick={() => paginate(currentPage - 1)}
+                disabled={currentPage === 1}
+              />
+              {Array.from(
+                { length: Math.ceil(filteredBooks.length / booksPerPage) },
+                (_, i) => (
+                  <Pagination.Item
+                    key={i}
+                    active={i + 1 === currentPage}
+                    onClick={() => paginate(i + 1)}
+                  >
+                    {i + 1}
+                  </Pagination.Item>
+                )
+              )}
+              <Pagination.Next
+                onClick={() => paginate(currentPage + 1)}
+                disabled={
+                  currentPage === Math.ceil(filteredBooks.length / booksPerPage)
+                }
+              />
             </Pagination>
           )}
         </Container>
         <div>
-          <Link to='/addbooks'>
-            <BiBookAdd style={{ position: "fixed", top: "600px", right: "25px", color: 'black', borderRadius: "100%", backgroundColor: "white", padding: "10px", width: "60px", height: "60px", boxShadow: "0px 0px 10px rgba(0, 0, 0, 1)" }} />
+          <Link to="/addbooks">
+            <BiBookAdd
+              style={{
+                position: "fixed",
+                top: "600px",
+                right: "25px",
+                color: "black",
+                borderRadius: "100%",
+                backgroundColor: "white",
+                padding: "10px",
+                width: "60px",
+                height: "60px",
+                boxShadow: "0px 0px 10px rgba(0, 0, 0, 1)",
+              }}
+            />
           </Link>
         </div>
         {showModal && selectedBook && (
-          <ViewTextbook show={showModal} handleClose={() => setShowModal(false)} book={selectedBook} />
+          <ViewTextbook
+            show={showModal}
+            handleClose={() => setShowModal(false)}
+            book={selectedBook}
+          />
         )}
       </Container>
     </div>
