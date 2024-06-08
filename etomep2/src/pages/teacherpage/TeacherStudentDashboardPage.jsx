76462a5245ba6b85471df3fc1b2af77a953e83fa
Anjana Paul <anjanapaul0614@gmail.com>
2024-06-08 17:00:46 +0530

    partially completed teacher student dashboard

--- etomep2/src/pages/teacherpage/TeacherStudentDashboardPage.jsx
+++ etomep2/src/pages/teacherpage/TeacherStudentDashboardPage.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+import MainHeader from '../../components/menus/common/MainHeader/';
 import StudentDashboard from '../../components/teacher/studentdashboard/StudentDashboard'
 
 function TeacherStudentDashboardPage() {
   return (
     <div>
+        <MainHeader/>
         <StudentDashboard/>
     </div>
   )
