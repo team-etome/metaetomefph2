41606cb51b2f17932d7aed6f496528b27a386d6f
Adrid Sadanand <adridsadanand@gmail.com>
2024-06-15 15:24:54 +0530

    text editor

--- etomep2/src/components/teacher/teachertexteditor/TeacherTextEditor.jsx
+++ etomep2/src/components/teacher/teachertexteditor/TeacherTextEditor.jsx
@@ -1,9 +1,102 @@
-import React from 'react'
+import React, { useState } from "react";
+import { CKEditor } from "@ckeditor/ckeditor5-react";
+import ClassicEditor from "ckeditor5-build-classic-mathtype";
+import DOMPurify from "dompurify";
 
-function TeacherTextEditor() {
+import { MdFormatBold, MdFormatItalic } from "react-icons/md";
+import { CiTextAlignLeft,CiTextAlignCenter, CiTextAlignRight,CiImageOn } from "react-icons/ci";
+
+export default function TeacherTextEditor() {
+  const [ckData, setCkData] = useState("");
+  
+  const createMarkup = (html) => {
+    return { __html: DOMPurify.sanitize(html) };
+  };
+
+  const [editorValue, changeEditorValue] = useState('');
+  const [selectionStart, setSelectionStart] = useState(0);
+  const [selectionEnd, setSelectionEnd] = useState(0);
+
+
+
+
+  const handleAlignmentLeft = () => {
+    document.execCommand('justifyLeft');
+  };
+
+  const handleAlignmentCenter = () => {
+    document.execCommand('justifyCenter');
+  };
+
+  const handleAlignmentRight = () => {
+    document.execCommand('justifyRight');
+  };
+
+  const handleInsertImage = (event) => {
+    const file = event.target.files[0];
+    const reader = new FileReader();
+    reader.onload = (e) => {
+      const imageUrl = e.target.result;
+      const img = `<img src="${imageUrl}" alt="inserted image" />`;
+      changeEditorValue(editorValue + img);
+    };
+    reader.readAsDataURL(file);
+  };
+
+
+
+  const handleEditorChange = (event) => {
+    changeEditorValue(event.target.value);
+  };
+
+  const handleSelectionChange = () => {
+    const selection = window.getSelection();
+    setSelectionStart(selection.anchorOffset);
+    setSelectionEnd(selection.focusOffset);
+  };
+
   return (
-    <div>TeacherTextEditor</div>
-  )
+    <React.Fragment>
+   
+      <CKEditor
+        editor={ClassicEditor}
+        config={{
+          toolbar: {
+            shouldNotGroupWhenFull: true,
+            items: [
+              "heading",
+              "outdent",
+              "indent",
+              "|",
+              "bold",
+              "italic",
+              "link",
+              "bulletedList",
+              "numberedList",
+              "insertTable",
+              "blockQuote",
+              "undo",
+              "redo",
+              "|",
+              "MathType",
+              "ChemType",
+            ],
+          },
+        }}
+        data={ckData}
+        onReady={(editor) => {
+          // Editor is ready to use
+        }}
+        onChange={(event, editor) => {
+          const data = editor.getData();
+          setCkData(data);
+        }}
+      />
+       
+      <div
+        style={{ whiteSpace: 'pre-wrap' }}
+        dangerouslySetInnerHTML={createMarkup(ckData)}
+      ></div>
+    </React.Fragment>
+  );
 }
-
-export default TeacherTextEditor
