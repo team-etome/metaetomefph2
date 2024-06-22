5213275c9ad074de534fa25c6f98a18652485eea
Afif <mk.afifmuhammad@gmail.com>
2024-06-22 11:57:14 +0530

    solved conflic in question editor

--- etomep2/src/components/teacher/studentdashboard/StudentDashboard.jsx
+++ etomep2/src/components/teacher/studentdashboard/StudentDashboard.jsx
@@ -84,7 +84,7 @@ function StudentDashboard() {
                     }}
                   >
                     <BsFilterRight style={{ height: "50px", width: "40px" }} />
-                  </div> */}
+                  </div> 
                 </div>
               </Col>
         </Row>
