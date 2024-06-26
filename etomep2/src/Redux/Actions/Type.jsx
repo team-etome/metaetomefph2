a3e009580239da2fc47ed2b53f5de7ae1265f7f6
Afif <mk.afifmuhammad@gmail.com>
2024-06-26 17:40:42 +0530

    completed question setting from teacher side

--- etomep2/src/Redux/Actions/Type.jsx
+++ etomep2/src/Redux/Actions/Type.jsx
@@ -2,4 +2,5 @@ export const ADMIN_INFO            =   'ADMIN_INFO'
 export const TEACHER_INFO          =   'TEACHER_INFO'
 export const ADMIN_CLASS_INFO      =   'ADMIN_CLASS_INFO'
 export const ADMIN_TEACHER_INFO    =   'ADMIN_TEACHER_INFO'
+export const EXAM_PAPER_INFO       =   'EXAM_PAPER_INFO'
 
--- etomep2/src/components/teacher/teacherquestiongenerator/QuestionGenerator.jsx
+++ etomep2/src/components/teacher/teacherquestiongenerator/QuestionGenerator.jsx
@@ -1,14 +1,15 @@
-import React, { useState, useCallback } from "react";
+import React, { useState, useEffect, useCallback, useRef } from "react";
 import { Container, Row, Col } from "react-bootstrap";
 import { DragDropContext, Droppable, Draggable } from "react-beautiful-dnd";
 import "../teacherquestiongenerator/questiongenerator.css";
-import { IoIosArrowBack } from "react-icons/io";
 import { IoAddCircleOutline } from "react-icons/io5";
 import { TbSection } from "react-icons/tb";
 import TeacherTextEditor from "../teachertexteditor/TeacherTextEditor";
 import { MdOutlineDelete } from "react-icons/md";
 import { PiDotsSix } from "react-icons/pi";
-import { Link } from "react-router-dom";
+import html2canvas from "html2canvas";
+import { useSelector } from "react-redux";
+import axios from "axios";
 
 function QuestionGenerator() {
   const [subsections, setSubsections] = useState([
@@ -17,30 +18,37 @@ function QuestionGenerator() {
       questions: [{ question: "", answer: "", points: 5, showAnswer: false }],
     },
   ]);
+
   const [currentSubsectionIndex, setCurrentSubsectionIndex] = useState(0);
-  const [finalizedQuestions, setFinalizedQuestions] = useState([]);
+  const [exportedData, setExportedData] = useState([]);
+  const questionRefs = useRef([]);
+  const answerRefs = useRef([]);
+  const [triggerExport, setTriggerExport] = useState(false);
+
+
+  const APIURL         = useSelector(state => state.APIURL.url);
+  const exampaperinfo  = useSelector(state => state.exampaperinfo);
+
+  const exam_id = exampaperinfo.exampaperinfo?.id
+  console.log(exam_id,"exam id")
+
+
+  console.log(exampaperinfo,"exampaperinfo reducer")
+
+  useEffect(() => {
+    if (triggerExport) {
+      setTimeout(() => {
+        setTriggerExport(false);
+        exportQuestionsToJson();
+      }, 500);
+    }
+  }, [triggerExport]);
 
 
 
-
-  const finalizeQuestion = (subsectionIndex, questionIndex) => {
-    const question = subsections[subsectionIndex].questions[questionIndex];
-    setFinalizedQuestions([...finalizedQuestions, question]);
-    const newSubsections = [...subsections];
-    newSubsections[subsectionIndex].questions.splice(questionIndex, 1);
-    setSubsections(newSubsections);
-
-    console.log("Question:", question.question);
-    console.log("Answer:", question.answer);
-  };
-
-
-
-  
   const handlePointsChange = (subsectionIndex, questionIndex, event) => {
     const newSubsections = [...subsections];
-    newSubsections[subsectionIndex].questions[questionIndex].points =
-      event.target.value;
+    newSubsections[subsectionIndex].questions[questionIndex].points = event.target.value;
     setSubsections(newSubsections);
   };
 
@@ -67,16 +75,13 @@ function QuestionGenerator() {
 
   const removeQuestion = (subsectionIndex, questionIndex) => {
     const newSubsections = [...subsections];
-    newSubsections[subsectionIndex].questions = newSubsections[
-      subsectionIndex
-    ].questions.filter((_, i) => i !== questionIndex);
+    newSubsections[subsectionIndex].questions = newSubsections[subsectionIndex].questions.filter((_, i) => i !== questionIndex);
     setSubsections(newSubsections);
   };
 
   const toggleAnswerKey = (subsectionIndex, questionIndex) => {
     const newSubsections = [...subsections];
-    newSubsections[subsectionIndex].questions[questionIndex].showAnswer =
-      !newSubsections[subsectionIndex].questions[questionIndex].showAnswer;
+    newSubsections[subsectionIndex].questions[questionIndex].showAnswer = !newSubsections[subsectionIndex].questions[questionIndex].showAnswer;
     setSubsections(newSubsections);
   };
 
@@ -85,17 +90,11 @@ function QuestionGenerator() {
 
     const { source, destination } = result;
     const sourceSubsectionIndex = parseInt(source.droppableId.split("-")[1]);
-    const destinationSubsectionIndex = parseInt(
-      destination.droppableId.split("-")[1]
-    );
+    const destinationSubsectionIndex = parseInt(destination.droppableId.split("-")[1]);
 
-    const sourceQuestions = Array.from(
-      subsections[sourceSubsectionIndex].questions
-    );
+    const sourceQuestions = Array.from(subsections[sourceSubsectionIndex].questions);
     const [removed] = sourceQuestions.splice(source.index, 1);
-    const destinationQuestions = Array.from(
-      subsections[destinationSubsectionIndex].questions
-    );
+    const destinationQuestions = Array.from(subsections[destinationSubsectionIndex].questions);
     destinationQuestions.splice(destination.index, 0, removed);
 
     const newSubsections = [...subsections];
@@ -124,6 +123,78 @@ function QuestionGenerator() {
     [subsections]
   );
 
+  const captureElement = async (element) => {
+    if (!element) {
+      console.error("Element not found");
+      return "Element not captured";
+    }
+    try {
+      const canvas = await html2canvas(element);
+      const dataUrl = canvas.toDataURL("image/png");
+      console.log("Capture successful", dataUrl);
+      return dataUrl;
+    } catch (error) {
+      console.error("Error capturing element:", error);
+      return "Error capturing element";
+    }
+  };
+
+  const exportQuestionsToJson = async () => {
+    console.log("Capturing questions and answers as images...");
+
+    const sectionsWithImages = await Promise.all(
+      subsections.map(async (subsection, sectionIndex) => {
+        const questionsWithImages = await Promise.all(
+          subsection.questions.map(async (q, questionIndex) => {
+            const questionElement = questionRefs.current[sectionIndex]?.[questionIndex]?.getEditorRef();
+            const answerElement = answerRefs.current[sectionIndex]?.[questionIndex]?.getEditorRef();
+
+            console.log(questionElement, "question Element "); // Debug log
+            console.log(answerElement, "answer Element "); // Debug log
+
+            const questionImage = questionElement ? await captureElement(questionElement) : "Image not captured";
+            const answerImage = answerElement ? await captureElement(answerElement) : "Image not captured";
+
+            return {
+              question: questionImage,
+              answer: answerImage,
+              marks: q.points,
+            };
+          })
+        );
+
+        return {
+          sectionName: subsection.name,
+          questions: questionsWithImages,
+        };
+      })
+    );
+
+    setExportedData(sectionsWithImages);
+    console.log("dataaaaaassssss", JSON.stringify(sectionsWithImages, null, 2));
+    sendToBackend(sectionsWithImages);
+  };
+
+
+
+  const sendToBackend = async (sectionsWithImages) => {
+    const data = {
+      question_id: exam_id,
+      data: sectionsWithImages
+    };
+  
+    try {
+      const response = await axios.post(`${APIURL}/api/questionpapersetting`, data);
+      console.log('Data successfully sent to the backend:', response.data);
+    } catch (error) {
+      console.error('Error sending data to the backend:', error);
+    }
+  };
+
+  const handleExport = () => {
+    setTriggerExport(true); // Set the trigger to initiate the export
+  };
+
   return (
     <DragDropContext onDragEnd={onDragEnd}>
       <div className="question_generator">
@@ -132,23 +203,15 @@ function QuestionGenerator() {
             <h6>Subject Name</h6>
           </Col>
           <Col className="question_generator_header_submit">
-            <button>Submit</button>
+            <button onClick={handleExport}>Export Questions</button>
           </Col>
         </Row>
         <Row>
           <div className="text-editor">
             {subsections.map((subsection, subsectionIndex) => (
-              <Droppable
-                key={subsectionIndex}
-                droppableId={`droppable-${subsectionIndex}`}
-                type="QUESTION"
-              >
+              <Droppable key={subsectionIndex} droppableId={`droppable-${subsectionIndex}`} type="QUESTION">
                 {(provided) => (
-                  <div
-                    ref={provided.innerRef}
-                    {...provided.droppableProps}
-                    className="subsection-container"
-                  >
+                  <div ref={provided.innerRef} {...provided.droppableProps} className="subsection-container">
                     <div className="subsection_header">
                       <textarea
                         className="subsection_textarea"
@@ -162,121 +225,84 @@ function QuestionGenerator() {
                         placeholder="Subsection Name"
                       />
                       <button onClick={() => removeSubsection(subsectionIndex)}>
-                        <MdOutlineDelete
-                          style={{ width: "32px", height: "32px" }}
-                        />
+                        <MdOutlineDelete style={{ width: "32px", height: "32px" }} />
                       </button>
                     </div>
-                    {subsection.questions.map((q, questionIndex) => (
-                      <Draggable
-                        key={questionIndex}
-                        draggableId={`draggable-${subsectionIndex}-${questionIndex}`}
-                        index={questionIndex}
-                      >
-                        {(provided) => (
-                          <div
-                            ref={provided.innerRef}
-                            {...provided.draggableProps}
-                            className="question-container"
-                          >
-                            <div className="teacher_question_header">
-                              <div className="teacher_question_number">
-                                <h6 style={{ fontSize: "20px" }}>
-                                  Q. {questionIndex + 1})
-                                </h6>
-                              </div>
-                              {/* <TeacherTextEditor placeholder="Type question here..." /> */}
-                              <TeacherTextEditor
-                                placeholder="Type question here..."
-                                editorData={q.question}
-                                setEditorData={(data) =>
-                                  handleEditorData(
-                                    subsectionIndex,
-                                    questionIndex,
-                                    "question",
-                                    data
-                                  )
-                                }
-                              />
-                            </div>
+                    {subsection.questions.map((q, questionIndex) => {
+                      // Initialize refs array for the subsection if not already initialized
+                      if (!questionRefs.current[subsectionIndex]) {
+                        questionRefs.current[subsectionIndex] = [];
+                      }
+                      if (!answerRefs.current[subsectionIndex]) {
+                        answerRefs.current[subsectionIndex] = [];
+                      }
 
-                            <div className="teacher_question_footer">
-                              <div
-                                className="answer-key"
-                                onClick={() =>
-                                  toggleAnswerKey(
-                                    subsectionIndex,
-                                    questionIndex
-                                  )
-                                }
-                              >
-                                Answer Key
-                              </div>
-                              <div></div>
-                            </div>
+                      return (
+                        <Draggable key={questionIndex} draggableId={`draggable-${subsectionIndex}-${questionIndex}`} index={questionIndex}>
+                          {(provided) => (
+                            <div ref={provided.innerRef} {...provided.draggableProps} className="question-container">
+                              <div className="teacher_question_header">
+                                <div className="teacher_question_number">
+                                  <h6 style={{ fontSize: "20px" }}>Q. {questionIndex + 1})</h6>
+                                </div>
 
-                            {q.showAnswer && (
-                              <div className="answer_editor_container">
                                 <TeacherTextEditor
-                                  placeholder="Type answer here..."
-                                  editorData={q.answer}
+                                  ref={(el) => (questionRefs.current[subsectionIndex][questionIndex] = el)}
+                                  placeholder="Type question here..."
+                                  editorData={q.question}
                                   setEditorData={(data) =>
-                                    handleEditorData(
-                                      subsectionIndex,
-                                      questionIndex,
-                                      "answer",
-                                      data
-                                    )
+                                    handleEditorData(subsectionIndex, questionIndex, "question", data)
                                   }
                                 />
-                                <div className="points-input">
-                                  <span>Mark</span>
-                                  <input
-                                    type="number"
-                                    value={q.points}
-                                    onChange={(e) =>
-                                      handlePointsChange(
-                                        subsectionIndex,
-                                        questionIndex,
-                                        e
-                                      )
+                              </div>
+
+                              <div className="teacher_question_footer">
+                                <div
+                                  className="answer-key"
+                                  onClick={() => toggleAnswerKey(subsectionIndex, questionIndex)}
+                                >
+                                  Answer Key
+                                </div>
+                                <div></div>
+                              </div>
+
+                              {q.showAnswer && (
+                                <div className="answer_editor_container">
+                                  <TeacherTextEditor
+                                    ref={(el) => (answerRefs.current[subsectionIndex][questionIndex] = el)}
+                                    placeholder="Type answer here..."
+                                    editorData={q.answer}
+                                    setEditorData={(data) =>
+                                      handleEditorData(subsectionIndex, questionIndex, "answer", data)
                                     }
                                   />
+                                  <div className="points-input">
+                                    <span>Mark</span>
+                                    <input
+                                      type="number"
+                                      value={q.points}
+                                      onChange={(e) => handlePointsChange(subsectionIndex, questionIndex, e)}
+                                    />
+                                  </div>
                                 </div>
-                              </div>
-                            )}
+                              )}
 
-                            <div className="question_actions">
-                              <div
-                                {...provided.dragHandleProps}
-                                className="drag_handle"
-                              >
-                                <PiDotsSix className="teacher_icon" />
+                              <div className="question_actions">
+                                <div {...provided.dragHandleProps} className="drag_handle">
+                                  <PiDotsSix className="teacher_icon" />
+                                </div>
+                                <button
+                                  className="delete_question_button"
+                                  onClick={() => removeQuestion(subsectionIndex, questionIndex)}
+                                >
+                                  <MdOutlineDelete className="teacher_icon" />
+                                </button>
                               </div>
-                              <button
-                                className="delete_question_button"
-                                onClick={() =>
-                                  removeQuestion(subsectionIndex, questionIndex)
-                                }
-                              >
-                                <MdOutlineDelete className="teacher_icon" />
-                              </button>
-                              <button
-                                onClick={() =>
-                                  finalizeQuestion(
-                                    subsectionIndex,
-                                    questionIndex
-                                  )
-                                }
-                                className="done_button"
-                              >
-                                Done
-                              </button>
                             </div>
-                          </div>
-                        )}
-                      </Draggable>
-                    ))}
+                          )}
+                        </Draggable>
+                      );
+                    })}
                     {provided.placeholder}
                   </div>
                 )}
