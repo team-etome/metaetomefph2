850c537ca78404bde70d6906fe7bc70bc63e20b6
Resolute Admin <development@resoluteindia.co.in>
2024-08-04 21:17:20 +0530

    dashboard and bug updates

--- etomep2/src/components/menus/header/Header.jsx
+++ etomep2/src/components/menus/header/Header.jsx
@@ -1,9 +1,9 @@
-import React, {useState, useEffect} from 'react';
+import React, { useState, useEffect } from 'react';
 import etomelogo from "../../../assets/etomelogo.png";
 import { Navbar, Container, Nav, Form, Button } from "react-bootstrap";
 import '../header/header.css';
 import { RxHamburgerMenu } from "react-icons/rx";
-import { FiBell } from 'react-icons/fi';
+// import { FiBell } from 'react-icons/fi';
 import amritha from "../../../assets/amritha.png";
 import MobileSidebar from '../sidebar/MobileSidebar';
 import MobileNotification from '../../teacher/mobilenotification/MobileNotification';
@@ -20,9 +20,9 @@ function Header() {
     const handleCloseSidebar = () => {
         setSidebarVisible(false);
     };
-    const handleBellIconClick = () => {
-        setShowModal(true); // Show modal when bell icon is clicked
-    };
+    // const handleBellIconClick = () => {
+    //     setShowModal(true); // Show modal when bell icon is clicked
+    // };
     const handleCloseModal = () => {
         setShowModal(false); // Close modal
     };
@@ -45,44 +45,42 @@ function Header() {
 
     return (
         <>
-        <Navbar expand="lg"
-        className='header_container'>
-            <Container fluid>
+            <Navbar expand="lg"
+                className='header_container'>
+                <Container fluid>
 
-                <Navbar.Brand href="#" className='header' >
-                    <div className='header_menu' onClick={handleBurgerClick}>
-                        <RxHamburgerMenu />
-                    </div>
-                    <div className='header_logo'>
+                    <Navbar.Brand href="#" className='header' >
+                        <div className='header_menu' onClick={handleBurgerClick}>
+                            <RxHamburgerMenu />
+                        </div>
+                        <div className='header_logo'>
+                            <img
+                                src={etomelogo}
+                                alt="etome logo" />
+                        </div>
+
+                    </Navbar.Brand>
+                    <div className='header_institution'>
+                        <div>
+                            <h6 style={{ color: '#9DB2BF', fontSize: '17px' }}>Institute Name</h6>
+                            <p style={{ color: '#727272', fontSize: '12px' }}>Institutemail@gmail.com</p>
+                        </div>
                         <img
-                            src={etomelogo}
-                            alt="etome logo"/>
+                            src={amritha}
+                            alt="Profile"
+                            style={{
+                                width: "52px",
+                                height: "52px",
+                                borderRadius: "50%",
+                                marginRight: "30px",
+                            }}
+                        />
                     </div>
 
-                </Navbar.Brand>
-                <div className='header_institution'>
-                    <div>
-                        <h6 style={{ color: '#9DB2BF', fontSize: '17px' }}>Institute Name</h6>
-                        <p style={{ color: '#727272', fontSize: '12px' }}>Institutemail@gmail.com</p>
-                    </div>
-                    <img
-                        src={amritha}
-                        alt="Profile"
-                        style={{
-                            width: "52px",
-                            height: "52px",
-                            borderRadius: "50%",
-                            marginRight: "30px",
-                        }}
-                    />
-                </div>
-                <div className='notification_icon' onClick={handleBellIconClick}>
-                    <FiBell />
-                </div>
-            </Container>
-            <MobileSidebar show={sidebarVisible} onClose={handleCloseSidebar} />
-        </Navbar>
-        {showModal && <MobileNotification onClose={handleCloseModal} />}
+                </Container>
+                <MobileSidebar show={sidebarVisible} onClose={handleCloseSidebar} />
+            </Navbar>
+            {showModal && <MobileNotification onClose={handleCloseModal} />}
         </>
     );
 }
