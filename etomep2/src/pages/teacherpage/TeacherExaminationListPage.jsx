7cc4316c2ef926a9ca02a395933ae5e495445ed1
Afif <mk.afifmuhammad@gmail.com>
2024-08-09 12:22:07 +0530

    header changed in teacher side

--- etomep2/src/pages/teacherpage/TeacherExaminationListPage.jsx
+++ etomep2/src/pages/teacherpage/TeacherExaminationListPage.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import ExaminationDashboard from '../../components/teacher/teacherexamination/ExaminationDashboard'
 import MainHeader from '../../components/menus/common/MainHeader/';
+import TeacherH from '../../components/menus/header/TeacherH';
+import TeacherHeader from '../../components/menus/common/TeacherHeader';
 
 function TeacherExaminationListPage() {
   return (
     <div>
-        <MainHeader/>
+        <TeacherHeader/>
         <ExaminationDashboard/>
     </div>
   )
--- etomep2/src/pages/teacherpage/TeacherStudentDashboardPage.jsx
+++ etomep2/src/pages/teacherpage/TeacherStudentDashboardPage.jsx
@@ -2,12 +2,12 @@ import React from 'react'
 import MainHeader from '../../components/menus/common/MainHeader/';
 import StudentDashboard from '../../components/teacher/studentdashboard/StudentDashboard'
 import TeacherNavbar from '../../components/teacher/teachernavbar/TeacherNavbar';
+import TeacherHeader from '../../components/menus/common/TeacherHeader';
 
 function TeacherStudentDashboardPage() {
   return (
     <div>
-        <MainHeader/>
-        {/* <TeacherNavbar/> */}
+        <TeacherHeader/> 
         <StudentDashboard/>
     </div>
   )
