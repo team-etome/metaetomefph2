d646909e73ddc68013c4ec9353b6c11a550d173a
team-etome <150006519+team-etome@users.noreply.github.com>
2024-08-23 17:08:27 +0530

    Update vite.config.js

--- etomep2/vite.config.js
+++ etomep2/vite.config.js
@@ -1,14 +1,14 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  server: {
-    host: '0.0.0.0', // Listen on all addresses
-    port: 5173,      // Default port
-  },
   build: {
-    outDir: 'vite-build'  // Set your desired output directory here
-  }
-})
+    outDir: 'dist',  // This specifies that the build files should be output to the 'dist' directory
+  },
+  server: {
+    host: '0.0.0.0',
+    port: 5173,
+  },
+});
