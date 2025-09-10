d0c6b9c09f8905834c230f68d37912feffd66008
ankitetome07 <ankit.dalal@etome.co.in>
2025-09-10 10:25:07 +0530

    api url changed

--- etomep2/src/Redux/Reducers/ApiUrlReducers.jsx
+++ etomep2/src/Redux/Reducers/ApiUrlReducers.jsx
@@ -7,9 +7,9 @@ const initialState = {
 
 
 
-    // url :"https://www.etomeindia.com"
+    url :"https://www.etomeindia.com"
 
-    url :"http://192.168.1.23:8000"
+    // url :"http://192.168.1.16:8000"
 
 
 
